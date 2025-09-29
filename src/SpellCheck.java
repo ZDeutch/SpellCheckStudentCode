9a3986549bb785512bb5cace15e533269e0a4169
ZDeutch <zander.deutch@menloschool.org>
2025-09-29 11:32:38 -0700

    Began a more efficient algorithm

--- src/SpellCheck.java
+++ src/SpellCheck.java
@@ -10,7 +10,7 @@ import java.util.Set;
  * for Adventures in Algorithms
  * At Menlo School in Atherton, CA
  *
- * Completed by: [YOUR NAME HERE]
+ * Completed by: Zander Deutch
  * */
 
 public class SpellCheck {
@@ -24,34 +24,47 @@ public class SpellCheck {
      * @return String[] of all mispelled words in the order they appear in text. No duplicates.
      */
     public String[] checkWords(String[] text, String[] dictionary) {
-        ArrayList<String> incorrect = new ArrayList<String>();
-
-        for(int i = 0; i < text.length; i++) {
-            int low = 0;
-            int high = dictionary.length - 1;
-            boolean found = false;
-            while(low <= high) {
-                int middle = (low + high) / 2;
-
-                int comparison = text[i].compareTo(dictionary[middle]);
-
-                if(comparison == 0) {
-                    found = true;
-                    break;
-
-                } else if(comparison > 0) {
-                    low = middle + 1;
-                } else {
-                    high = middle - 1;
-                }
-            }
-            if(!found) {
-                if(!incorrect.contains(text[i])) {
-                    incorrect.add(text[i]);
-                }
+        for(int i = 0; i < dictionary.length; i++) {
+            int num_val = 0;
+            int word_sum = 0;
+            int pow_10 = 1;
+            for(int j = dictionary[i].length() - 1; j > 0; j--) {
+                char c = dictionary[i].charAt(j);
+                int aasci = (int) c;
+                word_sum += aasci * pow_10;
+                pow_10 *= 10;
             }
         }
-        String[] incorrectArray = incorrect.toArray(new String[0]);
-        return incorrectArray;
     }
+
+
+
+
+//        HashSet<String> incorrect2 = new HashSet<>();
+//
+//        for(int i = 0; i < text.length; i++) {
+//            int low = 0;
+//            int high = dictionary.length - 1;
+//            boolean found = false;
+//            while(low <= high) {
+//                int middle = (low + high) / 2;
+//
+//                int comparison = text[i].compareTo(dictionary[middle]);
+//
+//                if(comparison == 0) {
+//                    found = true;
+//                    break;
+//
+//                } else if(comparison > 0) {
+//                    low = middle + 1;
+//                } else {
+//                    high = middle - 1;
+//                }
+//            }
+//            if(!found) {
+//                incorrect2.add(text[i]);
+//            }
+//        }
+//        return incorrect2.toArray(new String[0]);
+//    }
 }
